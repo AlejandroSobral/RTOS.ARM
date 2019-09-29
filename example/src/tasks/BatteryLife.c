9772dd87069625b198ea1e6569339d7295480042
Facu <facuruder@gmail.com>
2019-09-29 08:37:11 -0300

    ADC funcionando. Se envía _ y devuelve por UART el valor en ADC0.

--- example/src/tasks/BatteryLife.c
+++ example/src/tasks/BatteryLife.c
@@ -9,9 +9,8 @@
 #include <stdio.h>
 
 // ------ Public variable ------------------------------------------
-uint16_t bufADC[LEN_BUF_ADC];
-int colaIn = 0;
-int colaOut = 0;
+uint16_t bufADC;
+int enviandoSerie = 0;
 
 
 // ------ Private variable -----------------------------------------
@@ -23,19 +22,66 @@ void ADC0_Init(void)
 	Chip_ADC_Init(LPC_ADC,&adc_setup);
 	Chip_ADC_EnableChannel(LPC_ADC,ADC_CH0,ENABLE);
 	Chip_ADC_ReadValue(LPC_ADC,ADC_CH0,&dummy);
+	Chip_ADC_SetStartMode(LPC_ADC,ADC_START_NOW,ADC_TRIGGERMODE_RISING);
 }
 
 void EstadoBateria(void)
 {
-	uint16_t val=0;
+	static int estado = 0;
+	int i;
+	static char msg[LEN_BUF];
+	uint32_t NivelBateria = 0;
+	float PorcentajeFloat = 0;
+	uint32_t PorcentajeInt = 0;
+
 	if(Chip_ADC_ReadStatus(LPC_ADC,0,ADC_DR_DONE_STAT))
 	{
-		Chip_ADC_ReadValue(LPC_ADC,0,&val);
-		bufADC[colaIn++]=val;
-		if(colaIn<LEN_BUF_ADC)
+		Chip_ADC_ReadValue(LPC_ADC,0,&bufADC);
+
+		enviandoSerie = 1;
+	}
+
+	if(!enviandoSerie)
+	{
+		if(Chip_UART_ReadLineStatus(LPC_UART1)&UART_LSR_RDR)
 		{
-			Chip_ADC_SetStartMode(LPC_ADC,ADC_START_NOW,ADC_TRIGGERMODE_RISING);
+			if(Chip_UART_ReadByte(LPC_UART1)=='_')
+			{
+				Chip_ADC_SetStartMode(LPC_ADC,ADC_START_NOW,ADC_TRIGGERMODE_RISING);
+			}
 		}
 	}
 
+	if(enviandoSerie)
+		{
+		if(estado==0)
+		{
+			NivelBateria = bufADC * 8;
+			PorcentajeFloat = (float)NivelBateria / (float)100;
+			PorcentajeInt = (int)PorcentajeFloat;
+			memset(msg,0,LEN_BUF);
+			if (PorcentajeInt > 200)
+			{
+				sprintf(msg,"Estable: %03d\r\n", PorcentajeInt);
+
+			}
+			else
+			{
+				SYSTEM_Perform_Safe_Shutdown();
+			}
+			estado++;
+
+
+			enviandoSerie=0;
+		}
+		else
+		{
+		if((Chip_UART_ReadLineStatus(LPC_UART1) & UART_LSR_THRE))
+		{
+		for(i=0;i<LEN_BUFF;i++)
+			Chip_UART_SendByte(LPC_UART1,msg[i]);
+		estado=0;
+		}
+		}
+	}
 }
--- example/src/tasks/BatteryLife.h
+++ example/src/tasks/BatteryLife.h
@@ -1,6 +1,7 @@
 #define ADC0_PORT		(0)
 #define ADC0_PIN		(23)
 #define LEN_BUF_ADC		(4096)
+#define LEN_BUFF		(14)
 
 // ------ Public constants -----------------------------------------
 
